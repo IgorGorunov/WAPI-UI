8708050028a4d00220f13c60fae8311206ee100a
IgorGorunov <94405761+IgorGorunov@users.noreply.github.com>
2023-11-10 11:40:38 +0200

    fix

---
+++ src/components/TitleColumn/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+interface TitleColumnProps {
+    title?: string;
+    width: string;
+    contentPosition: string;
+    textColor?: string;
+    cursor?: string;
+    childrenBefore?: React.ReactNode;
+    childrenAfter?: React.ReactNode;
+    children?: never[];
+}
+
+const TitleColumn: React.FC<TitleColumnProps> = ({ title, width, contentPosition, textColor, cursor, childrenBefore, childrenAfter, children}) => {
+    const style: React.CSSProperties = {
+        display: 'flex',
+        width: width,
+        justifyContent: contentPosition,
+        alignItems: contentPosition,
+        color: textColor,
+        cursor: cursor,
+        gap: 5,
+    };
+    return (
+        <div style={style}>
+            {childrenBefore}
+            {title && <span>{title}</span>}
+            {childrenAfter}
+        </div>
+    );
+};
+
+export default TitleColumn;
