dff9d5cec8f95d1d8cd02de13237f74d80369725
talisk21 <talisk@i.ua>
2023-11-22 17:08:10 +0200

    add Calendar to date custom date in custom date input

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,16 +2,30 @@ const formatDateToString = (date: Date) => {
     let d = new Date(date),
         month = "" + (d.getMonth() + 1),
         day = "" + d.getDate(),
-        year = d.getFullYear();
+        year = "" +d.getFullYear();
 
     if (month.length < 2) month = "0" + month;
     if (day.length < 2) day = "0" + day;
+    if (year.length === 1) year = "000"+year;
 
     return [year, month, day].join("-");
+};
+
+const formatDateToDisplayString = (date: Date) => {
+    let d = new Date(date),
+        month = "" + (d.getMonth() + 1),
+        day = "" + d.getDate(),
+        year = "" +d.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+    if (year.length === 1) year = "000"+year;
+
+    return [day, month, year].join(".");
 };
 
 const getFirstDayOfMonth = (date: Date) => {
     return new Date(date.getFullYear(), date.getMonth(), 1)
 }
 
-export {formatDateToString, getFirstDayOfMonth}
+export {formatDateToString, getFirstDayOfMonth,formatDateToDisplayString}
