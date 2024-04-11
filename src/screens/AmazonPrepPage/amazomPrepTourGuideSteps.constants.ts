5f48ab42053ba07cd5b713b97d994b4a3e94c436
talisk21 <talisk@i.ua>
2024-04-11 16:11:16 +0300

    fix user tour guide (fixed texts, fix issue with invoices)

--- src/screens/AmazonPrepPage/amazomPrepTourGuideSteps.constants.ts
+++ src/screens/AmazonPrepPage/amazomPrepTourGuideSteps.constants.ts
@@ -30,32 +30,5 @@ export const tourGuideStepsAmazonPrep: TourGuideStepType[] = [
     },
 ];
 
-
 export const tourGuideStepsAmazonPrepNoDocs: TourGuideStepType[] = tourGuideStepsAmazonPrep.slice(1);
 tourGuideStepsAmazonPrepNoDocs[0].disableBeacon = true;
-
-// export const tourGuideStepsAmazonPrepNoDocs: TourGuideStepType[] = [
-//     {
-//         target: '.filter',
-//         content: 'Here you can filter orders by parameters',
-//         disableBeacon: true,
-//     },
-//     {
-//         target: '.date-input-field',
-//         content: 'Here you can filter your orders by the period of time',
-//     },
-//     {
-//         target: '.search-block',
-//         content: 'Write data here to locate information on the list below',
-//     },
-//     {
-//         target: '.add-order',
-//         content: 'Here you can create an order by filling out the form',
-//         //disableBeacon: true,
-//     },
-//     {
-//         target: '.export-orders',
-//         content: `Here you can export orders into Excel
-//             Note: All filters will be applied into export file`,
-//     },
-// ]
