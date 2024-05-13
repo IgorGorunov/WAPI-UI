4e8abd1754a3bc30645e76c8a9825e4b08273023
talisk21 <talisk@i.ua>
2024-05-13 09:29:10 +0300

    fix privacy policy page title

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -5,7 +5,7 @@ export default function Login() {
     return (
         <>
             <Head>
-                <title>Cookie policy</title>
+                <title>Privacy policy</title>
                 <meta name="privacy-policy" content="privacy policy" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/logo.png" type="image/png"/>
