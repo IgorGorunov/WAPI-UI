0a85a0e5e28c90ad3550d528ad30fd21b6e3450f
talisk21 <talisk@i.ua>
2024-10-28 16:11:50 +0200

    fix lead-page small issues

--- src/screens/LeadPage/leadPageTourGuideSteps.constants.ts
+++ src/screens/LeadPage/leadPageTourGuideSteps.constants.ts
@@ -2,18 +2,23 @@ import {TourGuideStepType} from "@/types/tourGuide";
 
 export const tourGuideStepsLeads: TourGuideStepType[] = [
     {
+        target: '.tabs-block__tab-link.company-tab',
+        content: 'Watch an introduction about Wapi and Wapi system\n',
+        disableBeacon: true,
+    },
+    {
         target: '.tabs-block__tab-link.prices-tab',
         content: 'Explore our pricing here - you can see basic price lists',
-        disableBeacon: true,
     },
     {
         target: '.tabs-block__tab-link.legal-documentation-tab',
         content: 'Read a contract sample, fill in your contract details and click “Submit contract”',
     },
     {
-        target: '.tabs-block__tab-link.api-info-tab',
+        target: '.tabs-block__tab-link.integration-tab',
         content: 'Check the ways to integrate with WAPI',
     },
+
     // {
     //     target: '.tabs-block__tab-link.ui-tutorial-tab',
     //     content: 'Refer to the UI tutorial for assistance',
