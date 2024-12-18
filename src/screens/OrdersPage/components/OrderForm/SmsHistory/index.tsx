618740000ca564e7ef42af044bad30f7c465ab40
talisk21 <talisk@i.ua>
2024-12-18 10:54:38 +0200

    hide sms status in Order sms history (temporary)

--- src/screens/OrdersPage/components/OrderForm/SmsHistory/index.tsx
+++ src/screens/OrdersPage/components/OrderForm/SmsHistory/index.tsx
@@ -18,7 +18,7 @@ const SmsHistory: React.FC<PropsType> = ({ smsHistory }) => {
         <div className="order-sms-history">
             <div className="order-sms-history__header">
                 <div className='date-column'>Period</div>
-                <div className='column status-column'>Status</div>
+                {/*<div className='column status-column'>Status</div>*/}
                 <div className='column recipient-column'>Recipient</div>
                 <div className='column text-column'>Sms text</div>
 
@@ -33,14 +33,14 @@ const SmsHistory: React.FC<PropsType> = ({ smsHistory }) => {
                             }`}
                         >
                             <div className='date-column'>{formatDateTimeToStringWithDot(sms.smsPeriod)}</div>
-                            <div className='column status-column'>
-                                <span style={{
-                                    borderBottom: `2px solid ${getUnderlineColor(sms.smsStatus)}`,
-                                    display: 'inline-block',
-                                }}>
-                                    {sms.smsStatus}
-                                </span>
-                            </div>
+                            {/*<div className='column status-column'>*/}
+                            {/*    <span style={{*/}
+                            {/*        borderBottom: `2px solid ${getUnderlineColor(sms.smsStatus)}`,*/}
+                            {/*        display: 'inline-block',*/}
+                            {/*    }}>*/}
+                            {/*        {sms.smsStatus}*/}
+                            {/*    </span>*/}
+                            {/*</div>*/}
                             <div className='column recipient-column'>{sms.smsRecipient}</div>
                             <div className='column text-column'>{sms.smsText}</div>
                         </li>
