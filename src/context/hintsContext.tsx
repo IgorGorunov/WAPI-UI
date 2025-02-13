1f15ec23121ae592514bafa88139fd1b2dd9d61c
talisk21 <talisk@i.ua>
2025-02-13 10:40:05 +0200

    fix issue with hints in Inbounds - not show hints question in it was canceled 2 times before

--- src/context/hintsContext.tsx
+++ src/context/hintsContext.tsx
@@ -9,6 +9,8 @@ type hintsContextType = {
     visitedStockMovements: boolean;
     setStockMovementsAsVisited: (val:boolean) => void;
     addInboundsNumber: () => void;
+    cancelHintsNumber: number;
+    addCancelHintsNumber: ()=>void;
 };
 
 const HintsContext = createContext<hintsContextType>({} as hintsContextType);
@@ -20,14 +22,20 @@ const useHintsTracking = (): hintsContextType => {
 export const HintsTrackingProvider = (props: PropsWithChildren) => {
     const [visitedStockMovements, setVisitedStockMovements] = useState<boolean>(Boolean(Cookie.get('visited-stock-movements')) || false);
     const [visitedInboundsNumber, setVisitedInboundsNumber] = useState<number>(Number(Cookie.get('visited-inbounds-number')) || 0);
+    const [cancelHintsNumber, setCancelHintsNumber] = useState(Number(Cookie.get('inbound-hints-cancel-number')) || 0);
 
     const setStockMovementsAsVisited = (val: boolean) => {
         setVisitedStockMovements(val);
-        Cookie.set('visited-stock-movements', val.toString());
+        Cookie.set('visited-stock-movements', val.toString(), { expires: 730 });
     }
     const addInboundsNumber = () => {
         setVisitedInboundsNumber(prevState=>prevState+1);
-        Cookie.set('visited-inbounds-number', (visitedInboundsNumber+1).toString());
+        Cookie.set('visited-inbounds-number', (visitedInboundsNumber+1).toString(),{ expires: 730 });
+    }
+
+    const addCancelHintsNumber = () => {
+        setCancelHintsNumber(prevState=>prevState+1);
+        Cookie.set('inbound-hints-cancel-number', (visitedInboundsNumber+1).toString(), { expires: 730 });
     }
 
     return (
@@ -35,6 +43,8 @@ export const HintsTrackingProvider = (props: PropsWithChildren) => {
             visitedStockMovements,
             setStockMovementsAsVisited: setStockMovementsAsVisited,
             addInboundsNumber,
+            cancelHintsNumber,
+            addCancelHintsNumber
         }}>
             {props.children}
         </HintsContext.Provider>
