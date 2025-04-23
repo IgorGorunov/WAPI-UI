722814a7bba54a592ce171ad113f3b2acd25f165
talisk21 <talisk@i.ua>
2025-04-23 17:14:32 +0300

    fix issue with FAQ (try to match Sanity libraries versions to NextJS 14.2.28)

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {faqItemFields, faqQuestionGroupFields} from "@/sanity/fragments";
-import {createClient} from "next-sanity";
-import clientConfig from "@/sanity/config/client-config";
 import {FaqPageType} from "@/types/sanity/pagesTypes";
 import FaqPageScreen from '@/screens/FaqPage';
 import AuthChecker from "@/components/AuthChecker";
+import {client} from "@/sanity/sanity-utils";
+
 
 const FaqPage = ({ faqPageData }: { faqPageData: FaqPageType}) => {
     return (
@@ -14,30 +14,38 @@ const FaqPage = ({ faqPageData }: { faqPageData: FaqPageType}) => {
     );
 };
 
+
 // Fetch the FAQ block data from Sanity
 export async function getServerSideProps() {
 
-    const query = `
-  *[_type == "faqPage"][0]{
-    title,
-    "content": content[] {
-      ...,
-      _type == "reference" => @->{
-        ${faqQuestionGroupFields(0)},
-        ${faqItemFields},
-      }    
+    try {
+        const query = `
+          *[_type == "faqPage"][0]{
+            title,
+            "content": content[] {
+              ...,
+              _type == "reference" => @->{
+                ${faqQuestionGroupFields(0)},
+                ${faqItemFields},
+              }
+            }
+          }
+        `;
+        const faqPageData: FaqPageType = await client.fetch(query);
+
+        return {
+            props: {
+                faqPageData,
+            },
+        };
+
+    } catch (error) {
+        console.error("FAQ fetch error:", error);
+        return {
+            notFound: true
+        };
     }
-  }
-`;
 
-    const faqPageData: FaqPageType = await createClient(clientConfig).fetch(query);
-
-
-    return {
-        props: {
-            faqPageData,
-        },
-    };
 }
 
 export default FaqPage;
--- src/sanity/config/client-config.ts
+++ src/sanity/config/client-config.ts
@@ -2,6 +2,7 @@ const config = {
     projectId: "26uk05nn",
     dataset: "production",
     apiVersion: "2024-12-19",
+    useCdn: false,
 }
 
 export default config;
