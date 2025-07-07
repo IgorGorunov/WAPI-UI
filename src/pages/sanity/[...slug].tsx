fce23c489adcdfee596b91e8797082153d886314
talisk21 <talisk@i.ua>
2025-07-07 14:02:34 +0300

    fix issue with Sanity page render (try 2)

--- src/pages/sanity/[...slug].tsx
+++ src/pages/sanity/[...slug].tsx
@@ -1,20 +1,37 @@
-// src/pages/sanity/[...slug].tsx
-'use client'
+// // src/pages/sanity/[...slug].tsx
+// 'use client'
+//
+// import { NextStudio } from 'next-sanity/studio';
+// import config from "../../../sanity.config";
+// import AuthChecker from "@/components/AuthChecker";
+// import {useEffect, useState} from "react";
+//
+// export default function AdminPage() {
+//     const [isClient, setIsClient] = useState(false);
+//     useEffect(() => {
+//         setIsClient(true);
+//         console.log('is client')
+//     }, []);
+//     return (
+//         <AuthChecker isUser={true}>
+//             {isClient && <NextStudio config={config} />}
+//         </AuthChecker>
+//     )
+//
+// }
 
-import { NextStudio } from 'next-sanity/studio';
-import config from "../../../sanity.config";
-import AuthChecker from "@/components/AuthChecker";
-import {useEffect, useState} from "react";
+import dynamic from 'next/dynamic';
+import AuthChecker from '@/components/AuthChecker';
+
+const Studio = dynamic(
+    () => import('next-sanity/studio').then((mod) => () => <mod.NextStudio config={require('../../../sanity.config').default} />),
+    { ssr: false }
+);
 
 export default function AdminPage() {
-    const [isClient, setIsClient] = useState(false);
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
     return (
-        <AuthChecker isUser={true}>
-            {isClient && <NextStudio config={config} />}
-        </AuthChecker>
-    )
-
+        // <AuthChecker isUser={true}>
+            <Studio />
+        // </AuthChecker>
+    );
 }
