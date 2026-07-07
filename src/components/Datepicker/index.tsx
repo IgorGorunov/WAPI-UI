ff5353ef580537c7ada9a8a5e40033c0065b9690
talisk21 <talisk@i.ua>
2026-07-07 15:12:43 +0300

    feat: datepicker date input format (new component created)

--- src/components/Datepicker/index.tsx
+++ src/components/Datepicker/index.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { DateRange } from "react-date-range";
 import Button from "@/components/Button/Button";
 import { DateRangeType } from "@/types/dashboard";
 import Icon from "@/components/Icon";
-import { formatDateToString } from "@/utils/date";
+import { formatDateToDisplayString } from "@/utils/date";
 import Draggable from "react-draggable";
 import styles from "./styles.module.scss";
-
+import DateInputSegmented from "@/components/DateInputSegmented";
 
 type DatepickerPropsType = {
     initialRange: DateRangeType;
@@ -24,41 +24,53 @@ const Datepicker: React.FC<DatepickerPropsType> = ({ initialRange, onDateRangeSa
         },
     ]);
 
-    const refRangeStartPreview = useRef<HTMLInputElement>(null);
-    const refRangeEndPreview = useRef<HTMLInputElement>(null);
+    // const refRangeStartPreview = useRef<HTMLInputElement>(null);
+    // const refRangeEndPreview = useRef<HTMLInputElement>(null);
+    //
+    // useEffect(() => {
+    //     if (refRangeStartPreview?.current) {
+    //         refRangeStartPreview.current.defaultValue = formatDateToString(dateRange[0].startDate)
+    //     }
+    //     if (refRangeEndPreview?.current) {
+    //         refRangeEndPreview.current.defaultValue = formatDateToString(dateRange[0].endDate)
+    //     }
+    // }, []);
 
-    useEffect(() => {
-        if (refRangeStartPreview?.current) {
-            refRangeStartPreview.current.defaultValue = formatDateToString(dateRange[0].startDate)
-        }
-        if (refRangeEndPreview?.current) {
-            refRangeEndPreview.current.defaultValue = formatDateToString(dateRange[0].endDate)
-        }
-    }, []);
-
-    const handleStartDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const val = e?.target.value;
-        if (val) {
-            setDateRange(prevState => ([{ ...prevState[0], startDate: new Date(val) || prevState[0].startDate }]))
+    const parseAndSetDate = (val: string, isStart: boolean) => {
+        const parts = val.split('.');
+        if (parts.length === 3 && parts[2].length === 4) {
+            const day = parseInt(parts[0], 10);
+            const month = parseInt(parts[1], 10) - 1;
+            const year = parseInt(parts[2], 10);
+            const newDate = new Date(year, month, day);
+            if (!isNaN(newDate.getTime())) {
+                setDateRange(prevState => {
+                    const newState = { ...prevState[0] };
+                    if (isStart) newState.startDate = newDate;
+                    else newState.endDate = newDate;
+                    return [newState];
+                });
+            }
         }
     }
 
-    const handleEndDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const val = e?.target.value;
-        if (val) {
-            setDateRange(prevState => ([{ ...prevState[0], endDate: new Date(val) || prevState[0].endDate }]))
-        }
+    const handleStartDateChange = (val: string) => {
+        parseAndSetDate(val, true);
+    }
+
+    const handleEndDateChange = (val: string) => {
+        parseAndSetDate(val, false);
     }
 
     const handleSelect = (ranges: any) => {
         setDateRange([ranges.selection]);
 
-        if (refRangeStartPreview?.current) {
-            refRangeStartPreview.current.defaultValue = formatDateToString(ranges.selection.startDate)
-        }
-        if (refRangeEndPreview?.current) {
-            refRangeEndPreview.current.defaultValue = formatDateToString(ranges.selection.endDate)
-        }
+        // if (refRangeStartPreview?.current) {
+        //     refRangeStartPreview.current.defaultValue = formatDateToString(ranges.selection.startDate)
+        // }
+        // if (refRangeEndPreview?.current) {
+        //     refRangeEndPreview.current.defaultValue = formatDateToString(ranges.selection.endDate)
+        // }
     };
 
     const handleSave = () => {
@@ -85,14 +97,20 @@ const Datepicker: React.FC<DatepickerPropsType> = ({ initialRange, onDateRangeSa
                     <div className={styles['date-range-container']}>
                         <div className={styles['range-preview']}>
                             <div className={styles['range-preview-date__wrapper']}>
-                                <input type='date' id='date-range-start'
-                                    onChange={handleStartDateChange} ref={refRangeStartPreview} />
+                                {/*<input type='date' id='date-range-start'*/}
+                                {/*       onChange={handleStartDateChange} ref={refRangeStartPreview} />*/}
+                                <DateInputSegmented 
+                                    id='date-range-start'
+                                    dateStr={formatDateToDisplayString(dateRange[0].startDate)} 
+                                    onChange={handleStartDateChange} 
+                                />
                             </div>
                             <div className={styles['range-preview-date__wrapper']}>
-                                <input type='date' id='date-range-end'
-                                    onChange={handleEndDateChange}
-                                    ref={refRangeEndPreview}
-                                    onKeyDown={(e) => (e.key === 'Enter' ? handleSave() : null)}
+                                <DateInputSegmented 
+                                    id='date-range-end'
+                                    dateStr={formatDateToDisplayString(dateRange[0].endDate)} 
+                                    onChange={handleEndDateChange} 
+                                    onEnter={handleSave}
                                 />
                             </div>
                         </div>
